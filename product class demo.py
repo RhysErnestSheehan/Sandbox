61ea6bd8a0309fa7189853b9d491cc76077f4286
Rhys <rhys.e.sheehan@protonmail.com>
2021-09-06 14:52:11 +1000

    product class demo

---
+++ product class demo.py
@@ -0,0 +1,17 @@
+"""product class demo"""
+
+class Product:
+    def __init__(self):
+        self.
+
+
+
+P = Product("Phone", 340, False)
+print(p)
+if p.is_on_sale
+
+
+
+on_sale_products = [product for product in products if product.is_on_sale]
+
+
