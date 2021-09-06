edfafe19eab3fff18c30f71b6c930a58e2019898
Rhys <rhys.e.sheehan@protonmail.com>
2021-09-06 14:52:38 +1000

    Lecture 6 DTN

---
+++ Lecture 6 Do This Now.py
@@ -0,0 +1,5 @@
+products = [["Phone", 340, False], ["PC", 1420.95, True], ["Plant", 24.5, True]]
+
+on_sale_products = [product for product in products if product[2]]
+print(products)
+print(on_sale_products)
