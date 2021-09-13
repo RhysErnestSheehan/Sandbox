2e209da6f523b3baf19a7f690f0330b2cd3474f5
Rhys <rhys.e.sheehan@protonmail.com>
2021-09-13 14:16:44 +1000

    Lecture 8 DTN

---
+++ Lecture 8 DTN.py
@@ -0,0 +1,6 @@
+"""Lecture 8 DTN"""
+
+names_to_ages = {"Bill": 21, "Jane": 34, "Sven":
+def list_of_name(name_to_age):
+    name for name in name_to_age
+    name for name in name_to_age[name] >= 30
