2e1e9bc6b4486fbe0b49e3aeeb62f096e2a61ba7
Rhys <rhys.e.sheehan@protonmail.com>
2021-11-13 12:36:26 +1000

    Exam revisions questions

--- CP1404 Exam Revision.py
+++ CP1404 Exam Revision.py
@@ -1,4 +1,74 @@
 """CP1404 Exam Revision 2021"""
 
-# Ask the user for their age, then print the square of each number from that age down to zero
-age = input("What is your age: ")
+
+# 2. Ask the user for their age, then print the square of each number from that age down to zero
+# age = input("What is your age: ")
+
+# 3. Write a function that takes two integer input parameters, adds up the numbers between these (inclusive) and returns
+# the total. If the first number is not less than the second number it should return 0.
+# TODO
+# number_1 = input("Number: ")
+# number_2 = input("Number: ")
+
+# 6. Write a function to count the number of words in a string (ignoring punctuation), e.g.
+# s = "This is a sentence with words in it" print(count_words(s)) # displays 8
+
+def count_number_of_words():
+    string = input("Enter a string: ")
+    for words in string:
+        print()
+
+
+count_number_of_words()
+
+# 7. Write a function that takes in one integer parameter, n, and returns a list of n unique random numbers between
+# 1 and 10 * n
+# import random
+#
+#
+# def list_random_numbers():
+#     n = input("Number: ")
+#     print(random.randint(1, 10, n))
+#
+#
+# list_random_numbers()
+
+# 8. Write a program that asks the user for a string then displays every letter in the string twice. E.g. if they input
+# "hello exam" it should display "hheelllloo  eexxaamm"
+
+# string = input("Enter a string: ")
+# for char in string:
+#     print(char * 2)
+
+# 9. Explain how a programmer would choose whether to use a while loop or a for loop and give examples of each.
+# A programmer would choose a while loop if they wanted something to happen while something else happened for example
+# while an input in black print invalid input
+# They would choose a for loop if they wanted something to happen for something in something else, for example for
+# place in places
+
+# 10. Describe the following terms: scope, exception, version control, commit, push, pull, merge, pull request,
+# virtual machine, bytecode, interpreter, abstraction, encapsulation, polymorphism, inheritance, instance, object,
+# class, method, attribute, class hierarchy, recursion,
+
+# 2. Exceptions are a form of error that you can build into your code.
+# 3. Version Control is taking snapshots of your code as you make changes so your can refer to them later or roll back
+# your code to a previous version if something goes wrong.
+# 4. A commit is adding something to version control.
+# 5. A push is when you sync your version control to a remote source from your computer.
+# 6. A pull is when your sync version control to your computer from a remote source
+# 7. A merge is when you merge two version control repositories into one.
+# 8. A pull request is when someone requests you
+# 9. virtual machine is a virtualised version of an operating system.
+# 10. bytecode is a form instruction set designed to be executed by a software interpreter.
+# 11. interpreter is a computer program that executes code
+# 12. abstraction
+# 13. encapsulation is when you hide the details of a program to make it clearer and easier to change later
+# 14. polymorphism means one message is sent to any object and it responds based on what type of object it is
+# 15. Inheritance is when a class inherits something from another class
+# 16. A Instance is a new version of a class object
+# 17. AN object is a new instance of the class
+# 18. A class is a file with code in it that can be reused in other files
+# 19. Methods are similar to functions except they are tied to an object
+# 20. Attributes are the different properties that an object has.
+# 21. class hierarchy
+# 22. recursion is when something calls itself. Like a function calling it self.
